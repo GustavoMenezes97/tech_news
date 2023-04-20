8457ea0a918cdfe52471b1e6c26ca8962bf55845
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2023-04-20 18:15:36 -0300

    Feat: Conclui o requisito 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -37,4 +37,13 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    content_list = search_news({
+        "category": {"$regex": category, "$options": "i"}
+    })
+
+    formated_content_list = []
+
+    for content in content_list:
+        formated_content_list.append((content["title"], content["url"]))
+
+    return formated_content_list
